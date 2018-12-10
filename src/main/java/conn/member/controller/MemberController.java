5ab477d4f10a22fca62621ec69e979655240a9df
unknown <rwggad@naver.com>
2018-12-10 16:10:49 +0900

    cooment add

--- src/main/java/conn/member/controller/MemberController.java
+++ src/main/java/conn/member/controller/MemberController.java
@@ -4,11 +4,9 @@ import conn.member.Member;
 import conn.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller

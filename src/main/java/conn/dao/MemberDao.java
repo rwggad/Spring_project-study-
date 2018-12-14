bd48dfdf1ede008aad318253224cdf96a2702ab8
unknown <rwggad@naver.com>
2018-12-14 18:49:10 +0900

    jdbc connecting....

--- src/main/java/conn/dao/MemberDao.java
+++ src/main/java/conn/dao/MemberDao.java
@@ -4,6 +4,7 @@ import conn.Model.Member;
 import conn.interfaces.IMemberDao;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.HashMap;
 
 
@@ -14,36 +15,93 @@ import java.util.HashMap;
 @Repository
 public class MemberDao implements IMemberDao {
 
-    private HashMap<String, Member> memberdb;
+    /**
+     * HashMap -> JDBC 변경한다 */
 
-    public MemberDao() {
+    /**
+     * 오라클과 연결 설정 (드라이버 연결) */
+    private String driver = "oracle.jdbc.driver.OracleDriver";
+    private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private String id = "root";
+    private String pw = "root";
+    private Connection conn = null;
+    private PreparedStatement pstmt;
+    private ResultSet resultSet;
+
+   /*private HashMap<String, Member> memberdb;*/
+    /*public MemberDao() {
         this.memberdb = new HashMap<String, Member>();
-    }
-
+    }*/
     public Member select(Member member) {
-        if(memberdb.containsKey(member.getMemId())){ // 찾으려는 아이디 값이 있을 경우
-            return this.memberdb.get(member.getMemId());
-        }else{ // 없을 경우
-            return null;
+        Member selectMember = null;
+        try{
+            Class.forName(driver);
+            this.conn = DriverManager.getConnection(url, id, pw);
+
+            conn.prepareStatement("SELECT * FROM Member WHERE ?");
+            pstmt.setString(1, member.getMemId());
+            resultSet = pstmt.executeQuery();
+            if(resultSet.next()){ // 찾으려는 아이디 값이 있을 경우
+                selectMember = new Member();
+                while(resultSet.next()){
+                    selectMember.setMemId(resultSet.getString(1));
+                    selectMember.setMemId(resultSet.getString(2));
+                    selectMember.setMemId(resultSet.getString(3));
+                }
+            }
+            return selectMember;
+        }catch (Exception e){
+            e.printStackTrace();
         }
+        return selectMember; // 없을 경우 null 을 반환한다.
     }
 
     public int insert(Member member) {
-        if(!memberdb.containsValue(member.getMemId())){ // 삽입하려는 아이디가 db에 없을 때 삽입
-            this.memberdb.put(member.getMemId(), member);
-            return 1;
-        }else{ // 이미 있는 아이디일 경우
-            return -1;
+        if(select(member) == null){ // 삽입하려는 아이디가 db에 없을 때 삽입
+            try{
+                Class.forName(driver);
+                this.conn = DriverManager.getConnection(url, id, pw);
+
+                conn.prepareStatement("INSERT INTO Member VALUES (?, ?, ?)");
+                pstmt.setString(1, member.getMemId());
+                pstmt.setString(2, member.getMemPw());
+                pstmt.setString(3, member.getMemMail());
+                return pstmt.executeUpdate();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }
+        return -1; // 이미 있는 아이디 이다.
     }
 
     public int update(Member member) {
-        this.memberdb.put(member.getMemId(),member);
-        return 1;
+        try{
+            Class.forName(driver);
+            this.conn = DriverManager.getConnection(url, id, pw);
+
+            conn.prepareStatement("UPDATE Member SET memId = ?, memPw = ?, memMail = ? WHERE memId = ?");
+            pstmt.setString(1, member.getMemId());
+            pstmt.setString(2, member.getMemPw());
+            pstmt.setString(3, member.getMemMail());
+            pstmt.setString(4,member.getMemId());
+            return pstmt.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return -1; // error
     }
 
     public int delete(Member member) {
-        this.memberdb.remove(member.getMemId());
-        return 1;
+        try{
+            Class.forName(driver);
+            this.conn = DriverManager.getConnection(url, id, pw);
+
+            conn.prepareStatement("DELETE FROM Member WHERE memId = ?");
+            pstmt.setString(1, member.getMemId());
+            return pstmt.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return -1; //error
     }
 }
